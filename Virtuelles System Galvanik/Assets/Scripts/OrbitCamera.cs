caabe09ecdaf9446e9ca49410f6f174b9c362fe0
BenniKenobi <Bennedict.Schweimer@haw-hamburg.de>
2022-01-25 23:33:13 +0100

    Kamera eingeschränkt

--- Virtuelles System Galvanik/Assets/Scripts/OrbitCamera.cs
+++ Virtuelles System Galvanik/Assets/Scripts/OrbitCamera.cs
@@ -13,12 +13,15 @@ namespace haw.unitytutorium.w21
 
         [SerializeField] private float minVerticalRotation = -30.0f;
 
+        [SerializeField] private float maxHorizontalRotation = 30.0f;
+        [SerializeField] private float minHorizontalRotation = -30.0f;
+
         [Header("Zoom Limits")] [SerializeField]
         private float maxZoomDistance = 10.0f;
 
         [SerializeField] private float minZoomDistance = 4.0f;
 
-        [SerializeField] private float maxHorizontalRotation;
+        
 
         private void Start()
         {
@@ -40,38 +43,38 @@ namespace haw.unitytutorium.w21
 
         private void RotateAroundTarget()
         {
-            transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * rotationSpeed);
+            var transformPosition = transform.position;
+            var targetPosition = target.position;
+            var axisHelper = targetPosition - new Vector3(targetPosition.x - 10, targetPosition.y, targetPosition.z);
+            var from = targetPosition - new Vector3(transformPosition.x, targetPosition.y, transformPosition.z);
+            var to = targetPosition - transformPosition;
+
+            var currentHorizontalAngle = transformPosition.z < targetPosition.z
+               ? -Vector3.Angle(axisHelper, from)
+               : Vector3.Angle(axisHelper, from);
+            Debug.Log(currentHorizontalAngle);
+
+            checkXBoundaries(currentHorizontalAngle);
 
             #region Limit Vertical Rotation
 
-            var transformPosition = transform.position;
-            var targetPosition = target.position;
-
-            var from = targetPosition - new Vector3(transformPosition.x, targetPosition.y, transformPosition.z);
-            var to = targetPosition - transformPosition;
             var currentVerticalAngle = transformPosition.y < targetPosition.y
                 ? -Vector3.Angle(from, to)
                 : Vector3.Angle(from, to);
-       
+
             var nextVerticalRot = Input.GetAxis("Mouse Y") * rotationSpeed;
 
             Debug.DrawLine(targetPosition, new Vector3(transformPosition.x, targetPosition.y, transformPosition.z),
                 Color.magenta);
             Debug.DrawLine(targetPosition, transformPosition, Color.blue);
+            Debug.DrawLine(targetPosition, new Vector3(targetPosition.x - 10, targetPosition.y, targetPosition.z), Color.red);
 
             if (currentVerticalAngle + nextVerticalRot >= maxVerticalRotation ||
                 currentVerticalAngle + nextVerticalRot <= minVerticalRotation)
                 return;
 
-           
-
             #endregion
-
-            var fromHorizantal = targetPosition - new Vector3(transformPosition.x, targetPosition.y, transformPosition.z);
-            var toHorizontal = targetPosition - transformPosition;
-            var currentHorizontalAngle = transformPosition.y < targetPosition.y
-                ? -Vector3.Angle(from, to)
-                : Vector3.Angle(from, to);
+           
 
             transform.RotateAround(target.position, transform.right, nextVerticalRot);
         }
@@ -92,6 +95,15 @@ namespace haw.unitytutorium.w21
             transform.Translate(new Vector3(0, 0, scrollAmount), Space.Self);
         }
 
+        public void checkXBoundaries(float currentHorizontalAngle)
+        {
+            var nextHorizontalRot = Input.GetAxis("Mouse X") * rotationSpeed;
 
+            if (currentHorizontalAngle + nextHorizontalRot >= maxHorizontalRotation ||
+                currentHorizontalAngle + nextHorizontalRot <= minHorizontalRotation)
+                return;
+
+            transform.RotateAround(target.position, Vector3.up, nextHorizontalRot);
+        }
     }
 }
