2d6d46514c3cd27a6ef1f4a4f9364909401da571
BenniKenobi <Bennedict.Schweimer@haw-hamburg.de>
2022-02-08 19:22:42 +0100

    Diverse fixes, erneutes baken

---
+++ Virtuelles System Galvanik/Assets/Scripts/PowerBtnAnimationController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBtnAnimationController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void setInteger(int num)
+    {
+        animator.SetInteger("changeState", num);
+    }
+}
